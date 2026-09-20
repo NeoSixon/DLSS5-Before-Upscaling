6e4170283ea210be6c98a9ab837e94b80fc19a79
NeoSixon <97835606+NeoSixon@users.noreply.github.com>
2026-09-21 03:20:15 +1000

    Bump manager backend to v0.8.5 mgr30

--- standalone/renderer/home-compat.js
+++ standalone/renderer/home-compat.js
@@ -5,7 +5,7 @@
 // Library page and make every page transition reset the shared content scroller.
 (() => {
   const scroller = document.querySelector('.content');
-  const MANAGER_BACKEND_ID = '0.7.7-dlss5mgr29';
+  const MANAGER_BACKEND_ID = '0.8.5-dlss5mgr30';
 
   if (I18N?.en) {
     I18N.en.games = 'Library';
